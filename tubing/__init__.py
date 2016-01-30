afcc292568a35787ccf7dac75f6ab78f4d8b790f
Bob Corsaro <bob@embed.ly>
2016-01-30 09:52:40 -0500

    Fixes py3.x

--- tubing/__init__.py
+++ tubing/__init__.py
@@ -1,6 +1,6 @@
 import pkgutil
 try:
-    version = pkgutil.get_data(__name__, 'VERSION')
+    version = pkgutil.get_data(__name__, 'VERSION').decode('utf-8')
 except IOError:
     version = '9999'
 
