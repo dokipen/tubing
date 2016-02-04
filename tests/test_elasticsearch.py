edb12be482c9ce69174e12c1a692e12b15a1bac4
Bob Corsaro <bob@embed.ly>
2016-02-04 16:53:25 -0500

    Elasticsearch work on testcase

---
+++ tests/test_elasticsearch.py
@@ -0,0 +1,38 @@
+import unittest2 as unittest
+from tubing.ext import elasticsearch
+from tubing import sinks, sources, pipe
+
+EXPECTED = """{"update": {"_type": "test", "_id": "id0"}}
+{"doc": {"name": "id0"}, "doc_as_upsert": true}
+{"update": {"_type": "test-child", "_id": "id1", "parent": "id0"}}
+{"doc": {"name": "id1"}, "doc_as_upsert": false}
+"""
+
+class ElasticSearchTestCase(unittest.TestCase):
+    def test_es(self):
+        buffer0 =  sinks.BytesIOSink()
+        sink = elasticsearch.BulkBatcherSink(buffer0)
+
+        sink.write([elasticsearch.DocUpdate('id0', dict(name='id0'), 'test')])
+        sink.write([elasticsearch.DocUpdate('id1', dict(name='id1'), 'test-child', parent_esid='id0', doc_as_upsert=False)])
+        sink.done()
+
+        buffer0.seek(0)
+        self.assertEqual(EXPECTED, buffer0.getvalue())
+
+    def test_batching(self):
+        buffer0 =  sinks.BytesIOSink()
+        sink = elasticsearch.BulkBatcherSink(buffer0, batch_size=50)
+
+        def make_du(i):
+            esid = 'id{}'.format(i)
+            return elasticsearch.DocUpdate(
+                esid=esid,
+                doc=dict(name=esid),
+                doc_type='test',
+            )
+
+        sink.write([make_du(i) for i in xrange(0,150)])
+        sink.done()
+
+        self.assertEqual(301, len(buffer0.getvalue().split("\n")))
--- tubing/ext/elasticsearch.py
+++ tubing/ext/elasticsearch.py
@@ -11,32 +11,55 @@ from tubing import sinks
 logger = logging.getLogger('tubing.ext.elasticsearch')
 
 
-class BulkUpdateBatcherSink(sinks.ProxySink):
+class DocUpdate(object):
+    """
+    DocUpdate is an ElasticSearch document update object. It is meant to be used with
+    BulkBatcher and returns an action and update.
+    """
+    def __init__(self, esid, doc, doc_type, parent_esid=None, doc_as_upsert=True):
+        """
+        esid is an elasticsearch _id
+        parent_esid is the elasticsearch _id of parent if parent exists
+        doc is a dict
+        """
+        self.doc = doc
+        self.esid = esid
+        self.parent_esid = parent_esid
+        self.doc_as_upsert = doc_as_upsert
+        self.doc_type = doc_type
+
+    def action(self):
+        action = dict(
+            update=dict(
+                 _id=self.esid,
+                 _type=self.doc_type,
+            ),
+        )
+        if self.parent_esid:
+            action['update']['parent'] = self.parent_esid
+        return json.dumps(action)
+
+    def update(self):
+        return json.dumps(dict(
+            doc_as_upsert=self.doc_as_upsert,
+            doc=self.doc,
+        ))
+
+
+
+class BulkBatcherSink(sinks.ProxySink):
     """
     Creates bulk updates for Elastic Search.  This is an object stream handler
-    and expects objects with index() and to_dict() methods implemented.
+    and expects DocUpdate objects.
     """
     def __init__(self, sink, batch_size=7500):
         self.sink = sink
         self.batch = []
         self.batch_size = batch_size
 
-    def action(self, doc):
-        return json.dumps(dict(
-            update=dict(
-                _id=doc.index(),
-            ),
-        ))
-
-    def update(self, doc):
-        return json.dumps(dict(
-            doc_as_upsert=True,
-            doc=doc.to_dict(),
-        ))
-
     def write(self, docs):
         for doc in docs:
-            self.batch.append("{}\n{}\n".format(self.action(doc), self.update(doc)))
+            self.batch.append("{}\n{}\n".format(doc.action(), doc.update()))
             if len(self.batch) > self.batch_size:
                 batch = ''.join(self.batch[:self.batch_size])
                 self.batch = self.batch[self.batch_size:]
@@ -44,25 +67,25 @@ class BulkUpdateBatcherSink(sinks.ProxySink):
 
     def done(self):
         self.sink.write(''.join(self.batch))
-        return super(BulkUpdateBatcherSink, self).done()
+        return super(BulkBatcherSink, self).done()
 
 
-class BulkSink(sinks.BaseSink):
+class BulkSink(sinks.BaseSink): # pragma: no cover
     """
     Elastic search bulk writer.
     """
-    def __init__(self, base, index_name, doc_type, username, password):
+    def __init__(self, base, index_name, username, password):
         self.base = base
         self.index_name = index_name
-        self.doc_type = doc_type
         self.username = username
         self.password = password
 
     def _url(self):
-        return "{}/{}/{}/_bulk".format(self.base, self.index_name, self.doc_type)
+        return "{}/{}/_bulk".format(self.base, self.index_name)
 
     def _auth(self):
-        return self.username, self.password
+        if self.username or self.password:
+            return self.username, self.password
 
     def write(self, chunk):
         logger.debug("POSTING: {}".format(chunk))
