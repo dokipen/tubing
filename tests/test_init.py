4307c9e28afff8f76dd3075311a46f5e4227856b
Bob Corsaro <bob@embed.ly>
2016-02-08 11:15:37 -0500

    Fixes tests

--- tests/test_init.py
+++ tests/test_init.py
@@ -6,5 +6,5 @@ import unittest2 as unittest
 class InitTestCase(unittest.TestCase):
 
     def test_version(self):
-        self.assertRegexpMatches(tubing.version, "^0\.0\.1.*")
-        self.assertRegexpMatches(tubing.__version__, "0\.0\.1.*")
+        self.assertRegexpMatches(tubing.version, "^0\.0\.2.*")
+        self.assertRegexpMatches(tubing.__version__, "0\.0\.2*")
